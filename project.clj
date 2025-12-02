4760f97f86350c5c9983478656dbcbcdade33817
Kyle Kingsbury <aphyr@aphyr.com>
2025-12-02 16:14:03 -0600

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject jepsen.nats "0.1.0-SNAPSHOT"
+(defproject jepsen.nats "0.1.0"
   :description "Jepsen tests for the NATS Jetstream queuing system"
   :url "https://github.com/jepsen-io/nats"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.3"]
-                 [jepsen "0.3.10-SNAPSHOT"
+                 [jepsen "0.3.10"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind
                                com.fasterxml.jackson.core/jackson-annotations
                                com.fasterxml.jackson.core/jackson-core]]
                  [io.jepsen/antithesis "0.1.0-SNAPSHOT"]
-                 [io.nats/jnats "2.24.0"]
+                 [io.nats/jnats "2.24.1"]
                  ; We're stuck on this version because Antithesis only works
                  ; with Jackson ~2.2.3, and this version of Cheshire is close
                  ; enough to be compatible
